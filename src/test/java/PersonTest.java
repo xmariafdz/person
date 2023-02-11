7a89b6c8f1e5af686f601485fae62a4ae0f89a7b
xmariafdz <maria05082002@gmail.com>
2023-02-11 15:14:35 +0100

    Add some more tests

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -14,9 +14,35 @@ public class PersonTest {
         person = new Person();
     }
     @Test
-    void meanAgeOfZeroPersons(){
+    void meanAgeForZeroPersons(){
         double[] obtainedValue = person.averageAgePerGender(new ArrayList<>());
         double[] expectedValue = {0,0};
+
+        assertArrayEquals(obtainedValue,expectedValue);
+    }
+
+    @Test
+    void meanAgeForOnePerson(){
+        List<Person> personList = new ArrayList<>(1);
+        personList.add(new Person("María",20,"Female"));
+
+        double[] obtainedValue = person.averageAgePerGender(personList);
+        double[] expectedValue = {0,20};
+
+        assertArrayEquals(obtainedValue,expectedValue);
+    }
+
+    @Test
+    void meanAgeForFivePersons(){
+        List<Person> personList = new ArrayList<>(5);
+        personList.add(new Person("María",20,"Female"));
+        personList.add(new Person("John", 25, "Male"));
+        personList.add(new Person("Jane", 30, "Female"));
+        personList.add(new Person("Jim", 35, "Male"));
+        personList.add(new Person("Joan", 40, "Female"));
+
+        double[] obtainedValue = person.averageAgePerGender(personList);
+        double[] expectedValue = {30,30};
 
         assertArrayEquals(obtainedValue,expectedValue);
     }
